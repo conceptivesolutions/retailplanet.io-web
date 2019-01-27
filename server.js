9c3fc44e25e5fb392373eae2627e7dcb41504fee
w.glanzer <wernerglanzer@googlemail.com>
2019-01-27 22:22:01 +0100

    Frontend: Produktsuche funktioniert nicht mehr -> server.js

--- server.js
+++ server.js
@@ -2,28 +2,44 @@ const next = require('next');
 const nextAuth = require('next-auth');
 const nextAuthConfig = require('./next-auth.config');
 const proxy = require('http-proxy-middleware');
+const dev = process.env.NODE_ENV !== 'production';
 
 // Initialize Next.js
-const nextApp = next({dir: '.', dev: (process.env.NODE_ENV !== 'production')});
+const nextApp = next({dir: '.', dev: dev});
 
 // Add next-auth to next app
-nextApp.prepare()
-    .then(async () => {
-        // Load configuration and return config object
-        const nextAuthOptions = await nextAuthConfig();
+nextApp
+    .prepare()
+    .then(() => {
+      return nextAuthConfig()
+    })
+    .then(nextAuthOptions => {
+      // Don't pass a port to NextAuth!
+      if (nextAuthOptions.port)
+        delete nextAuthOptions.port;
+      return nextAuth(nextApp, nextAuthOptions)
+    })
+    .then(nextAuthApp => {
+      // Get instance of Express from NextAuth instance
+      const expressApp = nextAuthApp.expressApp;
 
-        // Pass Next.js App instance and NextAuth options to NextAuth
-        const nextAuthApp = await nextAuth(nextApp, nextAuthOptions);
+      if (dev)
+        expressApp.use('/api', proxy({target: 'http://localhost:8080', changeOrigin: true}));
+      else
+        expressApp.use('/api', proxy({target: 'http://' + process.env.BACKEND_PORT_8080_TCP_ADDR + ':8080', changeOrigin: true}));
 
-        const expressApp = nextAuthApp.expressApp;
-        if (process.env.NODE_ENV !== 'production')
-            expressApp.use('/api', proxy({target: 'http://localhost:8080', changeOrigin: true}));
-        else
-            expressApp.use('/api', proxy({target: 'http://' + process.env.BACKEND_PORT_8080_TCP_ADDR + ':8080', changeOrigin: true}));
+      // Default catch-all handler to allow Next.js to handle all other routes
+      expressApp.all('*', (req, res) => {
+        let nextRequestHandler = nextApp.getRequestHandler();
+        return nextRequestHandler(req, res)
+      });
 
-      console.log(`Ready on http://localhost:3000`)
+      expressApp.listen(3000, err => {
+        if (err) throw err;
+        console.log('> Ready on http://localhost:3000')
+      });
     })
     .catch(err => {
-        console.log('An error occurred, unable to start the server');
-        console.log(err)
+      console.log('An error occurred, unable to start the server');
+      console.log(err)
     });
