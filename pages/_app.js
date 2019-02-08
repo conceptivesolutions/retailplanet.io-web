a9c67693e906308c6eaa001dd9c930f984611dc6
w.glanzer <wernerglanzer@googlemail.com>
2019-02-08 21:31:26 +0100

    Frontend: Favicon für alle gängigen Browser, mit "https://realfavicongenerator.net"

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,19 @@ export default class MyApp extends App
     return (
         <React.Fragment>
           <Head>
-            <title>retailplanet.io</title>
+            <title>retailplanet.io - buy from a local store</title>
+            <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png"/>
+            <link rel="manifest" href="/static/favicons/site.webmanifest"/>
+            <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5"/>
+            <link rel="shortcut icon" href="/static/favicons/favicon.ico"/>
+            <meta name="apple-mobile-web-app-title" content="retailplanet.io"/>
+            <meta name="application-name" content="retailplanet.io"/>
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="msapplication-TileImage" content="/static/favicons/mstile-144x144.png"/>
+            <meta name="msapplication-config" content="/static/favicons/browserconfig.xml"/>
+            <meta name="theme-color" content="#ffffff"/>
             <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"/>
           </Head>
