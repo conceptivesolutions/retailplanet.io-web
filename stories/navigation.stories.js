8c3fcae97225b14dd5e2595c9ccf34e52d1cb039
w.glanzer <wernerglanzer@googlemail.com>
2019-02-10 12:04:50 +0100

    Storybook: Höhe des PageHeaders setzen

--- stories/navigation.stories.js
+++ stories/navigation.stories.js
@@ -3,16 +3,24 @@ import {storiesOf} from '@storybook/react';
 import Header from '../components/navigation/Header';
 import Footer from '../components/navigation/Footer';
 import '../static/styles/styles.scss';
+import css from './navigation.stories.scss'
+import SearchHeader from "../components/search/SearchHeader";
 
 storiesOf('Navigation', module)
     .add('Page Header with Logo', () => (
-        <Header withLogo/>
+        <div className={css.headerPane}>
+          <Header withLogo/>
+        </div>
     ))
     .add('Page Header with Logo (fixed top)', () => (
-        <Header withLogo fixedTop/>
+        <div className={css.headerPane}>
+          <Header withLogo fixedTop/>
+        </div>
     ))
     .add('Page Header with Searchbar', () => (
-        <Header withLogo withSearch/>
+        <div className={css.headerPane}>
+          <SearchHeader withLogo query="Encoded%20Value"/>
+        </div>
     ))
     .add('Page Footer', () => (
         <Footer/>
