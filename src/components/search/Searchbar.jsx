02d5854dd8167172dccc3cf870fe6653f7cc8941
w.glanzer <w.glanzer@conceptive.io>
2019-02-22 15:18:02 +0100

    Collapsed Searchbar to a stateless component

--- src/components/search/Searchbar.jsx
+++ src/components/search/Searchbar.jsx
@@ -10,33 +10,29 @@ import { runSearch } from '../../reducers/searchReducer';
 /**
  * @author w.glanzer, 14.01.2019
  */
-class Searchbar extends React.Component {
-  render() {
-    return (
-      <Form onSubmit={(e) => {
-        e.preventDefault();
-        this.props.onExecute(e.target.query.value);
-      }}
-      >
-        <InputGroup className={this.props.className}>
-          <FormControl
-            type="Query"
-            placeholder="Durchsuchen Sie über 6.000.000 Produkte"
-            name="query"
-            className={`${css.searchField}`}
-            defaultValue={this.props.query}
-          />
-          <InputGroup.Append>
-            <Button variant="primary" type="submit" className={`${css.searchButton} px-4 border-0`}>
-              Suchen
-            </Button>
-          </InputGroup.Append>
-        </InputGroup>
-        {this.props.children}
-      </Form>
-    );
-  }
-}
+const Searchbar = props => (
+  <Form onSubmit={(e) => {
+    e.preventDefault();
+    props.onExecute(e.target.query.value);
+  }}
+  >
+    <InputGroup className={props.className}>
+      <FormControl
+        type="Query"
+        placeholder="Durchsuchen Sie über 6.000.000 Produkte"
+        name="query"
+        className={`${css.searchField}`}
+        defaultValue={props.query}
+      />
+      <InputGroup.Append>
+        <Button variant="primary" type="submit" className={`${css.searchButton} px-4 border-0`}>
+          Suchen
+        </Button>
+      </InputGroup.Append>
+    </InputGroup>
+    {props.children}
+  </Form>
+);
 
 const mapStateToProps = state => ({
   query: parse(state.router.location.search).query,
