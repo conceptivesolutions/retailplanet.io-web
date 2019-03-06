752bc89aee966c990367805382a7bf80677e2f14
w.glanzer <wernerglanzer@googlemail.com>
2019-03-07 00:26:06 +0100

    Added "trace:true"-Flag to debug the stacktrace of an redux-action

--- src/store.js
+++ src/store.js
@@ -1,13 +1,20 @@
 /* eslint-disable import/prefer-default-export,no-param-reassign */
-import {applyMiddleware, combineReducers, createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
-import {createRouterMiddleware, initialRouterState, routerReducer} from 'connected-next-router';
-import {i18nState} from 'redux-i18n';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { createRouterMiddleware, initialRouterState, routerReducer } from 'connected-next-router';
+import { i18nState } from 'redux-i18n';
 import thunkMiddleware from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import searchReducer from './reducers/searchReducer';
+import userReducer from './reducers/userReducer';
 
 const routerMiddleware = createRouterMiddleware();
+
+const composeEnhancers = composeWithDevTools({
+  realtime: true,
+  trace: true,
+  traceLimit: 25,
+});
 
 export function makeStore(initialState = {}, options) {
   if (options && options.asPath) initialState.router = initialRouterState(options.asPath);
@@ -19,6 +26,6 @@ export function makeStore(initialState = {}, options) {
       i18nState,
     }),
     initialState,
-    composeWithDevTools(applyMiddleware(routerMiddleware, thunkMiddleware, promise)),
+    composeEnhancers(applyMiddleware(routerMiddleware, thunkMiddleware, promise)),
   );
 }
