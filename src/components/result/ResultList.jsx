cfc494ff9fac7a3e5e8ac8b39db9f57be9955e36
w.glanzer <wernerglanzer@googlemail.com>
2019-03-31 12:30:20 +0200

    Fixed Pagination not clipping over ResultList in Safari

--- src/components/result/ResultList.jsx
+++ src/components/result/ResultList.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import ResultItem from './ResultItem';
+import css from './ResultList.scss';
 
 /**
  * @author w.glanzer, 10.02.2019
@@ -10,7 +11,7 @@ const ResultList = (props) => {
   if (!props.results || props.results.length === 0)
     return <span>No data</span>;
   return (
-    <div className="d-flex flex-wrap mb-3 mr-3">
+    <div className={`${css.item} mb-3 mr-3`}>
       {/* eslint-disable-next-line react/no-array-index-key */}
       {props.results.map((pItem, pIndex) => (
         <ResultItem className="ml-3 mt-3" data={pItem} key={pIndex} />
