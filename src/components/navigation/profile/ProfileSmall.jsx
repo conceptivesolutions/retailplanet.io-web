029b289db2431c430e1630c65884494963257a8e
w.glanzer <wernerglanzer@googlemail.com>
2019-04-21 12:06:48 +0200

    Included avatar in profile component

--- src/components/navigation/profile/ProfileSmall.jsx
+++ src/components/navigation/profile/ProfileSmall.jsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { withRouter } from 'next/router';
 import css from './ProfileSmall.scss';
 import ANavDropdown from '../dropdown/ANavDropdown';
-import bearerFetch from '../../../auth/bearerFetch';
 
 /**
  * props.session = Die aktuelle Session (optional)
@@ -12,58 +11,20 @@ import bearerFetch from '../../../auth/bearerFetch';
  * @author w.glanzer, 27.01.2019
  */
 class ProfileSmall extends React.Component {
-  state = {
-    filled: false,
-    avatar: null,
-  };
 
   constructor(props) {
     super(props);
-    this.updateUserProfileInfos = this.updateUserProfileInfos.bind(this);
-    this.renderAvatar = this.renderAvatar.bind(this);
     this.createUserComp = this.createUserComp.bind(this);
-  }
-
-  componentDidUpdate() {
-    this.updateUserProfileInfos();
-  }
-
-  /**
-   * Fetches new user informations, and transfers them into current state.
-   * Does nothing, if the state is already set
-   */
-  updateUserProfileInfos() {
-    const { user } = this.props;
-    if (user && !this.state.filled)
-      bearerFetch('/api/profile', user)
-        .then(pResult => pResult.json())
-        .then(json => this.setState({
-          avatar: json.avatar,
-          filled: true,
-        }));
-    else if (!user && this.state.filled)
-      this.setState({
-        filled: false,
-        avatar: null,
-      });
   }
 
   createUserComp() {
     return (
       <React.Fragment>
-        {this.renderAvatar()}
+        <img src={`/api/profile/avatar/${encodeURIComponent(this.props.user.profile.id)}.png`}
+          alt="avatar" className={`${css.profilepic} mr-2 rounded-circle`} />
         {this.props.user.profile.username}
       </React.Fragment>
     );
-  }
-
-  /**
-   * Renders the current users avatar
-   */
-  renderAvatar() {
-    if (this.state.avatar)
-      return <img src={`data:image/png;base64, ${this.state.avatar}`} alt="avatar" className={`${css.profilepic} mr-2 rounded-circle`} />;
-    return <React.Fragment />;
   }
 
   render() {
