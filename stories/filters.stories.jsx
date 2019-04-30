8b6b0f89b4159bf9972f985dc1300418f539acd0
w.glanzer <wernerglanzer@googlemail.com>
2019-04-30 14:01:53 +0200

    Added GeoFilter to filters story

--- stories/filters.stories.jsx
+++ stories/filters.stories.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withI18N, withReduxStore } from '../.storybook/decorators';
+import GeoFilter from '../src/components/filters/geo/GeoFilter';
+import './filters.stories.scss';
 
 storiesOf('Filters', module)
   .addDecorator(withI18N)
-  .addDecorator(withReduxStore());
+  .addDecorator(withReduxStore())
+  .add('Geo Location', () => (<GeoFilter />));
